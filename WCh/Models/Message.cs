e6034c9b26281240ce8853e32a1b9d3a345f8082
Ulweyn <61630451+Ulweyn@users.noreply.github.com>
2020-04-18 21:07:50 +0300

    First steps

---
+++ WCh/Models/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WCh.Models
+{
+    public class Message
+    {
+        public int IdUser { get; set; }
+        public string Content { get; set; }
+        public DateTime Moment { get; set; }
+    }
+}
